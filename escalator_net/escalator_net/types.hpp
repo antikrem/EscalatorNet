1304da74a81259c67f7f594f0630ceb23266f642
Chalinda Dinusha Rodrigo <chalindarodrigo@gmail.com>
2020-04-11 05:11:57 +1000

    Added a constants header

---
+++ escalator_net/escalator_net/types.hpp
@@ -0,0 +1,24 @@
+/* General type defs
+*/
+
+#ifndef __TYPES__
+#define __TYPES__
+
+#include <stdint.h>
+
+// Unsigned Integers with fixed size
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+
+// Unsigned Integers with fixed size
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+
+// Shortened standard uint declaration
+typedef unsigned int uint;
+
+#endif
