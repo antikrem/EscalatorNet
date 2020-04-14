33288e259c6e04d1e6740db14ec4b9ed0698da31
Chalinda Dinusha Rodrigo <chalindarodrigo@gmail.com>
2020-04-14 12:30:35 +1000

    Added network print

--- escalator_net/escalator_net/network.hpp
+++ escalator_net/escalator_net/network.hpp
@@ -5,28 +5,38 @@
 
 #include "layer.hpp"
 
-// Feed structure for initialising network
+ // Forward declaraction of Node class for use by outstream operator declaraction
+template <typename T>
+class Network;
 
- /* Gradient descent optimisation results
-  * No invariants, totally public
+// Forward declaration of stream output
+template <typename T> static std::ostream& operator<<(std::ostream& os, const Network<T>& n);
+
+ /* Contains a full network
   */
 template <typename T>
-struct Network {
+class Network {
 private:
 	// Threshold for total network cost
 	const T C_THRESH = T(0.01);
 
-	// number of iterations for late optimisation
-	uint count = 0;
-
 	// Upperbound for total count
 	const uint ITER_MAX = 200;
+
+	// number of iterations for last optimisation
+	uint count = 0;
+
+	// cost for last iteraion
+	T cost = T(0);
 
 	// List of hidden layers
 	std::vector<Layer<T>> layers;
 
 	// Last output from forward propogation
 	VMatrix<T> lastPrediction = VMatrix <T>(1, 1);
+
+	// Declare outstream print as a friend ))
+	template <typename T> friend std::ostream& operator<<(std::ostream& os, const Network<T>& n);
 
 public:
 	// Generate network with given number of hidden layers
@@ -82,7 +92,7 @@ public:
 
 	// Optimises this network given input and observed
 	void optimiseNetwork(const VMatrix<T>& input, const VMatrix<T>& YObs, bool print = false) {
-		T cost = T(C_THRESH + T(1));
+		cost = T(C_THRESH + T(1));
 
 		count = 0;
 
@@ -99,10 +109,30 @@ public:
 		}
 	}
 
+
+
 	// Makes prediction with given input
 	VMatrix<T> makePrediction(const VMatrix<T>& input) {
 		return forwardPropogate(input);
 	}
+
+	// Makes prediction with nice output
+	void predict(const VMatrix<T>& input) {
+		std::cout << "Input:" << std::endl;
+		std::cout << input.transpose() << std::endl;
+		std::cout << "output:" << std::endl;
+		std::cout << makePrediction(input).transpose() << std::endl;
+	}
 };
+
+template <typename T>
+static std::ostream& operator<<(std::ostream& os, const Network<T>& n)
+{
+	std::cout << "NETWORK of " << n.layers.size() << " layers" << std::endl;
+	std::cout << "Converged: " << (n.cost < n.C_THRESH ? "TRUE" : "FALSE") << std::endl;
+	std::cout << "Iterations: " << n.count << std::endl;
+	std::cout << "Cost: " << n.cost;
+	return os;
+}
 
 #endif
