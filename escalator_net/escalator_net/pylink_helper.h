936c617bf3693ecad8d2a4575896708f79b7e88f
Chalinda Dinusha Rodrigo <chalindarodrigo@gmail.com>
2020-05-22 02:24:26 +1000

    Fixed PY_STRING failing on string literal parameters

--- escalator_net/escalator_net/pylink_helper.h
+++ escalator_net/escalator_net/pylink_helper.h
@@ -4,6 +4,6 @@
 #ifndef __PYLINK_HELPER__
 #define __PYLINK_HELPER__
 
-#define PY_STRING(x) Py_BuildValue("s", (x).data())
+#define PY_STRING(x) Py_BuildValue("s", (std::string(x)).data())
 
 #endif
